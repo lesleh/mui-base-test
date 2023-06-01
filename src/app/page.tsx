9c6083f0b6d7019acb6df687f7c15abfe0cede7f
Leslie Hoare <iam@lesleh.co.uk>
2023-06-01 10:19:34 +0100

    Add CTM button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import { MenuItem, TextField, ThemeProvider } from "@mui/material";
+import Button from "@mui/material/Button";
+import { theme } from "./theme";
+
+const currencies = [
+  {
+    value: "USD",
+    label: "$",
+  },
+  {
+    value: "EUR",
+    label: "€",
+  },
+  {
+    value: "BTC",
+    label: "฿",
+  },
+  {
+    value: "JPY",
+    label: "¥",
+  },
+];
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <Button variant="outlined" color="secondary">
+          Clicky Button
+        </Button>
+
+        {/* <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          defaultValue="EUR"
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField> */}
+      </main>
+    </ThemeProvider>
+  );
 }
---
+++ src/app/theme.tsx
@@ -0,0 +1,48 @@
+import createTheme from "@mui/material/styles/createTheme";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4632d8",
+    },
+  },
+});
+
+theme = createTheme(theme, {
+  components: {
+    // Name of the component
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+        variant: "contained",
+      },
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          borderRadius: 9999,
+          backgroundColor: "#fff",
+          color: theme.palette.primary.main,
+          border: `2px solid ${theme.palette.primary.main}`,
+          textTransform: "none",
+          fontSize: 20,
+          fontWeight: "bold",
+          boxShadow: "none",
+          "&:hover": {
+            boxShadow: "none",
+            backgroundColor: theme.palette.primary.main,
+            color: "#fff",
+            border: "1px solid transparent",
+          },
+          "&:active": {
+            boxShadow: "none",
+            backgroundColor: theme.palette.primary.main,
+            borderColor: theme.palette.primary.main,
+          },
+        },
+      },
+    },
+  },
+});
+
+export { theme };
